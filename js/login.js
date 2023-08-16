2845d00d6d70f94f79cd7476f7b0a4d2fefd72cb
cbasoa <141192086+cbasoa@users.noreply.github.com>
2023-08-15 21:47:05 -0300

    Validación de los campos
    Se validan los campos por separado, más una alerta en general

--- js/login.js
+++ js/login.js
@@ -5,18 +5,33 @@ document.getElementById("boton").addEventListener("click", validarRegistro);
 //Validar Registro
 function validarRegistro(e){
     let correo = document.getElementById("floatingInput3").value;
-     let contraseña = document.getElementById("exampleInputPassword1").value;
-     const checkbox = document.getElementById("exampleCheck1");
-   
-     if(correo && contraseña.length >= 6 && checkbox.checked){
+    let contraseña = document.getElementById("exampleInputPassword1").value;
+    let alerta = document.getElementById("alerta");
+    const checkbox = document.getElementById("exampleCheck1");
+    let expresion = /\w+@+\w+\.+[a-z]/
+    
+     if(expresion.test(correo) && contraseña.length >= 6 && checkbox.checked){
        window.open("index.html");
+      }else if(!expresion.test(correo) && (contraseña.length >= 6 && checkbox.checked)) {
+        document.getElementById("obligatorio").style.display = 'block';
+        document.getElementById("alerta1").style.display = "block";
+        e.preventDefault();
+      }else if( contraseña.length < 6 && (expresion.test(correo) && checkbox.checked)  ){
+        document.getElementById("obligatorio1").style.display = 'block';
+        document.getElementById("alerta1").style.display = "block";
+        e.preventDefault();
+      }else if((expresion.test(correo) && contraseña.length >= 6) && !(checkbox.checked)){
+        document.getElementById("obligatorio2").style.display = 'block';
+        document.getElementById("alerta1").style.display = "block";
+            e.preventDefault();
+
       }else {
         document.getElementById("obligatorio").style.display = 'block';
         document.getElementById("obligatorio1").style.display = 'block';
         document.getElementById("obligatorio2").style.display = 'block';
+        document.getElementById("alerta1").style.display = "block";
             e.preventDefault();
 
-      }
-       }
-    
-    
+      }}
+
+
