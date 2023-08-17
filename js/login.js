6c8e42fa1165d8fec786150e89113670447a01ef
gcortazzo <guilleee94@gmail.com>
2023-08-16 21:48:43 -0300

    Redireccionar al login
    En caso de que no haya datos en localStorage redirecciona al login

---
+++ js/login.js
@@ -0,0 +1,53 @@
+<<<<<<< Updated upstream
+=======
+document.getElementById("boton").addEventListener("click", validarRegistro);
+
+//Validar Registro
+function validarRegistro(e) {
+  let correo = document.getElementById("floatingInput3").value;
+  let contraseña = document.getElementById("exampleInputPassword1").value;
+  let alerta = document.getElementById("alerta");
+  const checkbox = document.getElementById("exampleCheck1");
+  let expresion = /\w+@+\w+\.+[a-z]/;
+
+  if (expresion.test(correo) && contraseña.length >= 6 && checkbox.checked) {
+    localStorage.setItem("correo", correo);
+    localStorage.setItem("pass", contraseña);
+    window.open("index.html");
+  } else if (
+    !expresion.test(correo) &&
+    contraseña.length >= 6 &&
+    checkbox.checked
+  ) {
+    document.getElementById("obligatorio").style.display = "block";
+    document.getElementById("alerta1").style.display = "block";
+    e.preventDefault();
+  } else if (
+    contraseña.length < 6 &&
+    expresion.test(correo) &&
+    checkbox.checked
+  ) {
+    document.getElementById("obligatorio1").style.display = "block";
+    document.getElementById("alerta1").style.display = "block";
+    e.preventDefault();
+  } else if (
+    expresion.test(correo) &&
+    contraseña.length >= 6 &&
+    !checkbox.checked
+  ) {
+    document.getElementById("obligatorio2").style.display = "block";
+    document.getElementById("alerta1").style.display = "block";
+    e.preventDefault();
+  } else {
+    document.getElementById("obligatorio").style.display = "block";
+    document.getElementById("obligatorio1").style.display = "block";
+    document.getElementById("obligatorio2").style.display = "block";
+    document.getElementById("alerta1").style.display = "block";
+    e.preventDefault();
+
+    if (!localStorage.getItem("correo") || !localStorage.getItem("pass")) {
+      window.location.href = "login.html";
+    }
+  }
+}
+>>>>>>> Stashed changes
