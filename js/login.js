a27bd9ffe465fee37fe1003e4284eb7a03a2b96b
gcortazzo <guilleee94@gmail.com>
2023-08-16 22:16:10 -0300

    Redireccion al Login
    Cuando faltan datos en el localstorage redireccional al login.

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-
 document.getElementById("boton").addEventListener("click", validarRegistro);
 
 //Validar Registro
@@ -50,43 +49,13 @@ function validarRegistro(e) {
   }
 }
 
+document.addEventListener("keydown", function (event) {
+  // Verifica si la tecla presionada es la tecla "B" (puedes usar otra tecla si lo prefieres)
+  if (event.key === "b") {
+    // Borra todo el contenido del LocalStorage
+    localStorage.clear();
 
-document.getElementById("boton").addEventListener("click", validarRegistro);
-
-
-//Validar Registro
-function validarRegistro(e){
-    let correo = document.getElementById("floatingInput3").value;
-    let contraseña = document.getElementById("exampleInputPassword1").value;
-    let alerta = document.getElementById("alerta");
-    const checkbox = document.getElementById("exampleCheck1");
-    let expresion = /\w+@+\w+\.+[a-z]/
-    
-     if(expresion.test(correo) && contraseña.length >= 6 && checkbox.checked){
-      localStorage.setItem("correo", correo )
-      localStorage.setItem("pass", contraseña)
-       window.open("index.html");
-      }else if(!expresion.test(correo) && (contraseña.length >= 6 && checkbox.checked)) {
-        document.getElementById("obligatorio").style.display = 'block';
-        document.getElementById("alerta1").style.display = "block";
-        e.preventDefault();
-      }else if( contraseña.length < 6 && (expresion.test(correo) && checkbox.checked)  ){
-        document.getElementById("obligatorio1").style.display = 'block';
-        document.getElementById("alerta1").style.display = "block";
-        e.preventDefault();
-      }else if((expresion.test(correo) && contraseña.length >= 6) && !(checkbox.checked)){
-        document.getElementById("obligatorio2").style.display = 'block';
-        document.getElementById("alerta1").style.display = "block";
-            e.preventDefault();
-
-      }else {
-        document.getElementById("obligatorio").style.display = 'block';
-        document.getElementById("obligatorio1").style.display = 'block';
-        document.getElementById("obligatorio2").style.display = 'block';
-        document.getElementById("alerta1").style.display = "block";
-            e.preventDefault();
-
-      }}
-
-
-
+    // Muestra un mensaje de confirmación
+    alert("LocalStorage ha sido borrado.");
+  }
+});
