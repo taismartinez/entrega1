9fccd9889767963e97d1105db23fb02b33c0d55d
cbasoa <141192086+cbasoa@users.noreply.github.com>
2023-08-30 18:29:48 -0300

    Elimino los archivos
    Por error se subieron demás

--- cart.js
+++
@@ -1,5 +0,0 @@
-const email= document.getElementById("email");
-let userEmail= localStorage.getItem("correo");
-console.log(localStorage.getItem("correo"));
-email.innerHTML= userEmail;
-email.href="myprofile.html";
