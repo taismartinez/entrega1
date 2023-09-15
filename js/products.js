a0f9fcc33f5c2efc315480462c851351554a3997
taismartinez <tati06mar@gmail.com>
2023-09-15 20:32:59 -0300

    arreglol

--- js/products.js
+++ js/products.js
@@ -151,12 +151,3 @@ function selectedProduct(prod){
 
 showProducts();
 
-//Llamado a tipo de productos a traves de su categoria
-if (catid ==="101") {
-    document.getElementById("categories").innerHTML = "Autos";
-} else if (catid ==="102") {
-    document.getElementById("categories").innerHTML = "Juguetes";
-} else if (catid ==="103") {
-    document.getElementById("categories").innerHTML = "Muebles";
-}
-
