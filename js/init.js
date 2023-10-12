d8e0ab7126222c871830f2e08838e20c1f4b2023
cbasoa <141192086+cbasoa@users.noreply.github.com>
2023-10-11 22:04:56 -0300

    Cambios corregidos

--- js/init.js
+++ js/init.js
@@ -63,3 +63,75 @@ let userEmail= localStorage.getItem("correo");
 console.log(localStorage.getItem("correo"));
 email.innerHTML= userEmail;
 email.href="myprofile.html";
+
+
+
+//Modo noche
+document.addEventListener("DOMContentLoaded", function() {
+  const nightModeSwitch = document.getElementById("night-mode-switch");
+  const nightModeStylesheet = document.getElementById("night-mode-stylesheet");
+
+  // Función para activar o desactivar el modo nocturno
+  function toggleNightMode() {
+    nightModeStylesheet.disabled = !nightModeSwitch.checked;
+
+    // Cambiar el texto del interruptor según el modo actual
+    if (nightModeSwitch.checked) {
+      nightModeSwitch.nextElementSibling.textContent = "Modo Diurno";
+    } else {
+      nightModeSwitch.nextElementSibling.textContent = "Modo Nocturno";
+    }
+
+    // Guardar el estado del modo en localStorage
+    localStorage.setItem("nightMode", nightModeSwitch.checked);
+  }
+
+  // Agregar el evento de cambio para el interruptor
+  nightModeSwitch.addEventListener("change", toggleNightMode);
+
+  // Verificar el estado almacenado en localStorage al cargar la página
+  const storedNightMode = localStorage.getItem("nightMode");
+  if (storedNightMode === "true") {
+    nightModeSwitch.checked = true;
+  } else {
+    nightModeSwitch.checked = false;
+  }
+
+  // Inicializar el modo según el estado almacenado
+  toggleNightMode();
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const nightModeSwitch = document.getElementById("night-mode-switch");
+  const nightModeStylesheet = document.getElementById("night-mode-stylesheet");
+
+  // Función para activar o desactivar el modo nocturno
+  function toggleNightMode() {
+    nightModeStylesheet.disabled = !nightModeSwitch.checked;
+
+    // Cambiar el texto del interruptor según el modo actual
+    if (nightModeSwitch.checked) {
+      nightModeSwitch.nextElementSibling.textContent = "Modo Diurno";
+    } else {
+      nightModeSwitch.nextElementSibling.textContent = "Modo Nocturno";
+    }
+
+    // Guardar el estado del modo en localStorage
+    localStorage.setItem("nightMode", nightModeSwitch.checked);
+  }
+
+  // Agregar el evento de cambio para el interruptor
+  nightModeSwitch.addEventListener("change", toggleNightMode);
+
+  // Verificar el estado almacenado en localStorage al cargar la página
+  const storedNightMode = localStorage.getItem("nightMode");
+  if (storedNightMode === "true") {
+    nightModeSwitch.checked = true;
+  } else {
+    nightModeSwitch.checked = false;
+  }
+
+  // Inicializar el modo según el estado almacenado
+  toggleNightMode();
+});
